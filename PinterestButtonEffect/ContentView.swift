3c0eb869fc78565c46815b4f2f5fdd358612133c
David Lin <yoxisem544@gmail.com>
2023-10-02 12:35:08 +0800

    Initial Commit

---
+++ PinterestButtonEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PinterestButtonEffect
+//
+//  Created by David on 2023/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PinterestButtonEffect/PinterestButtonEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestButtonEffectApp.swift
+//  PinterestButtonEffect
+//
+//  Created by David on 2023/10/2.
+//
+
+import SwiftUI
+
+@main
+struct PinterestButtonEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
